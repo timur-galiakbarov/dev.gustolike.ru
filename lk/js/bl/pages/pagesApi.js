cca2b9f9ac697b0d66ca84897d8dce8033fe92ec
timur-galiakbarov <timur@aroona.ru>
2015-08-20 18:23:29 +0500

    Убрал лишние комменты

--- lk/js/bl/pages/pagesApi.js
+++ lk/js/bl/pages/pagesApi.js
@@ -6,7 +6,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
                 var defer = $.Deferred();
                 $http.get("/engine/controllers/pages/getList.php").success(function (data) {
                     defer.resolve({success: data});
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     defer.resolve({success: false});
                     console.log("Произошла ошибка получения списка акций");
                 });
@@ -15,7 +15,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
             getPage: function(code, callback){//Получение информации об акции
                 $http.get("/engine/controllers/pages/getPage.php?code="+code).success(function (data) {
                     if (callback) callback(data);
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     console.log("Произошла ошибка получения информации об акции");
                     if (callback) callback(data);
                 });
@@ -23,7 +23,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
             isMemberPage: function(code, callback){
                 $http.get("/engine/controllers/pages/isMemberPage.php?code="+code).success(function (data) {
                     if (callback) callback(data);
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     console.log("Произошла ошибка получения данных о вступлении в акцию");
                     if (callback) callback(data);
                 });
@@ -31,7 +31,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
             isMemberAction: function(id, flag, callback){
                 $http.post("/engine/controllers/pages/includeMember.php", {elementId: id, isMember: flag}).success(function (data) {
                     if (callback) callback(data);
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     console.log("Произошла ошибка при вступлении/выходе из акции");
                     if (callback) callback(data);
                 });
@@ -39,7 +39,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
             getMembersList: function(pageId, callback){
                 $http.get("/engine/controllers/pages/getMemberList.php?pageId="+pageId).success(function (data) {
                     if (callback) callback(data);
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     console.log("Произошла ошибка получения списка подписчиков акции");
                     if (callback) callback(data);
                 });
@@ -49,7 +49,7 @@ radPages.factory('pageServerApi', ['$http', function($http){
             getList: function(id, callback){
                 $http.get("/engine/controllers/pages/getPrizesList.php?id="+id).success(function (data) {
                     if (callback) callback(data);
-                }).error(function (data) {//эмуляция обращения к серверу
+                }).error(function (data) {
                     console.log("Произошла ошибка при получении списка подарков");
                     if (callback) callback(data);
                 });
